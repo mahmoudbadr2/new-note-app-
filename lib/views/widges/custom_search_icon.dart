5fbb68e9fa696102f35726e095b9178e3c1e1618
mahmoudbadr2 <mahmoudb731@gmail.com>
2025-11-22 22:26:12 +0200

    add custom app to edit view

--- lib/views/widges/custom_search_icon.dart
+++ lib/views/widges/custom_search_icon.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CustomSerchIcon extends StatelessWidget {
-  const CustomSerchIcon({super.key});
-
+  const CustomSerchIcon({super.key, required this.icon});
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,7 +12,7 @@ class CustomSerchIcon extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
         color: Colors.white.withOpacity(0.07),
       ),
-      child: const Center(child: Icon(Icons.search, size: 28)),
+      child: Center(child: Icon(icon, size: 28)),
     );
   }
 }
--- lib/views/widges/edit_view_body.dart
+++ lib/views/widges/edit_view_body.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widges/custom_app_bar.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(height: 50),
+          CustomAppBar(title: 'Edit Note', icon: Icons.check),
+        ],
+      ),
+    );
   }
 }
