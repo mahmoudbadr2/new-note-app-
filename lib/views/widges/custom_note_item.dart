cb6201815009dba12a9f72be637423886607c8aa
mahmoudbadr2 <mahmoudb731@gmail.com>
2025-11-22 22:15:52 +0200

    create edit note view

--- lib/views/widges/custom_note_item.dart
+++ lib/views/widges/custom_note_item.dart
@@ -1,47 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: Colors.amberAccent,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text(
-                'Build your career with mahmoud badr',
-                style: TextStyle(
-                  color: Colors.black.withValues(alpha: 0.5),
-                  fontSize: 18,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: Colors.amberAccent,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'Build your career with mahmoud badr',
+                  style: TextStyle(
+                    color: Colors.black.withValues(alpha: 0.5),
+                    fontSize: 18,
+                  ),
                 ),
               ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.delete, color: Colors.black, size: 28),
+              ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.delete, color: Colors.black, size: 28),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May21 , 2025',
+                style: TextStyle(color: Colors.black.withValues(alpha: 0.5)),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May21 , 2025',
-              style: TextStyle(color: Colors.black.withValues(alpha: 0.5)),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
